e3f0d3c5cac24f9f432082b2a0e094e9f9843757
Yasin <yasinasadi@hotmail.com>
2026-07-26 10:52:55 +0330

    Create: Sandbox created

---
+++ How to control.py
@@ -0,0 +1,10 @@
+
+
+
+for x in range(3):
+    for y in range(3):
+        for z in range(3):
+            print(x,y,z)
+            print(x,y,z,sep=",")
+            print(x,y,z,sep=" ")
+            print(x,y,z,sep="")
